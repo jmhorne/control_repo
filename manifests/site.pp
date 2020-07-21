653efd67574eec008c60794dc3c1bd1d55c398f0
jmhorne <65088686+jmhorne@users.noreply.github.com>
2020-07-21 10:58:37 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,11 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a line of text',
-    owner   => 'root',
-  }
   
-  file {'/root/README':
-    owner => 'root',
-  }
 }
